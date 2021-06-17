8d070697c90611d57d76bc9ef6f6ad0d728b3862
emanao <emanao@me.com>
2021-06-17 16:05:48 -0400

    Initial seed

--- shopping-tracker-backend/db/seeds.rb
+++ shopping-tracker-backend/db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Package.delete_all
+Order.delete_all
+Site.delete_all
+
+ActiveRecord::Base.connection.reset_pk_sequence!("package")
+ActiveRecord::Base.connection.reset_pk_sequence!("order")
+ActiveRecord::Base.connection.reset_pk_sequence!("site")
+
+pk1 = Package.create(name:"Water Bottles")
+order1 = Order.create(nr:123456)
+pk1.order = order1
+pk1.save
+site1 = Site.create(name: "Takeya")
+order1.site=site1
+order1.save
+
+
+
